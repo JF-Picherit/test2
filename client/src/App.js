3db25c3bac661d248121b7fe4a76547deda4ca13
JF-PS <jfps.dev21@gmail.com>
2021-10-14 11:25:16 +0200

    test1

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,16 @@
 import React from 'react';
-//import { Container } from '@material-ui/core';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar3';
-//import Navbar from './components/Navbar/Navbar';
 import Auth from './components/Auth/Auth2';
-//import Annonce from './components/Annonce/Annonce';
-import SocialCardDemo from './components/SocialCardDemo/SocialCardDemo';
-import SelectField from './components/Inputs/SelectField/SelectField';
-import TextField from './components/Inputs/TextField/TextField';
-
 
 const App = () => (
   <BrowserRouter>
-    {/* <Container maxWidth="lg"> */}
-      <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/auth" exact component={Auth} />
-        <Route path="/annonce" exact component={SocialCardDemo} />
-        <Route path="/select" exact component={SelectField} />
-        <Route path="/text" exact component={TextField} />
-      </Switch>
-    {/* </Container> */}
+    <Navbar />
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/auth" exact component={Auth} />
+    </Switch>
   </BrowserRouter>
 );
 
